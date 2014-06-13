56f2a8237ddc4fb83843f366cf3ca2e93c2cda22
Michael Sheedy <git@michaelsheedy.com>
2014-06-12 23:47:13 -0400

    DIB toolkit setup (mostly sass files)

--- tasks/options/sass.js
+++ tasks/options/sass.js
@@ -3,6 +3,18 @@ module.exports = {
 	// Grunt task to compile Sass SCSS to CSS
 	// https://github.com/gruntjs/grunt-contrib-sass
 
+  options: {              // Target options
+    precision: 6,
+    trace: false,
+    noCache: false,
+    cacheLocation: '<%= temp %>/.sass-cache',
+    loadPath: ["vendor/bower_components"],
+    // require: "susy",
+    bundleExec: true,
+    quiet: true,
+    // banner: "/* Test banner */", // Can't be used if you use the sourcemap option.
+  },
+
 	// Save to .sass.css for autoprefixer
     server: {
       options: {              // Target options
